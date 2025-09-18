1e69521f64a55fb646298205a8fb6dcf0696827f
Ty Schaub <tyschaub@Tys-MacBook-Air.local>
2025-09-18 15:15:03 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args)
+    {
+        int numToExamine = 2;
+        int modulus = numToExamine % 2;
+        if (modulus == 0) {
+            System.out.println(numToExamine + " is even");
+        } else {
+            System.out.println(numToExamine + " is odd");
+        }
+    }
+}
